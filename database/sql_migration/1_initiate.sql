12e3d9302a4f7defd80698c9966a3bac0ddb497d
rnwhyu <r_wahyuningtyas@yahoo.co.id>
2023-03-27 06:34:05 +0700

    update file sql

--- database/sql_migration/1_initiate.sql
+++ database/sql_migration/1_initiate.sql
@@ -2,23 +2,23 @@
 -- +migrate StatementBegin
 
 CREATE TABLE books(
-    id integer NOT NULL,
-    title character varying NOT NULL,
-    description character varying NOT NULL,
-    image_url character varying NOT NULL,
-    release_year integer NOT NULL,
-    price character varying NOT NULL,
-    total_page integer NOT NULL,
-    thickness character varying NOT NULL,
-    created_at timestamp without time zone DEFAULT now() NOT NULL,
-    updated_at timestamp without time zone DEFAULT now() NOT NULL,
+    id serial4 NOT NULL,
+    title VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
+    image_url VARCHAR NOT NULL,
+    release_year INT NOT NULL,
+    price VARCHAR NOT NULL,
+    total_page INT NOT NULL,
+    thickness VARCHAR NOT NULL,
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp DEFAULT now() NOT NULL,
     category_id integer NOT NULL
 );
 CREATE TABLE categories (
-    id integer NOT NULL,
-    name character varying NOT NULL,
-    created_at timestamp without time zone DEFAULT now() NOT NULL,
-    updated_at timestamp without time zone DEFAULT now() NOT NULL
+    id serial4 NOT NULL,
+    "name" VARCHAR NOT NULL,
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp DEFAULT now() NOT NULL
 );
 
 -- +migrate StatementEnd
