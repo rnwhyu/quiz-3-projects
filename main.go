41d3589ca04c1f866d2f61e6b030ce4fc92ab433
rnwhyu <r_wahyuningtyas@yahoo.co.id>
2023-03-27 07:22:11 +0700

    edit main.go

--- main.go
+++ main.go
@@ -2,15 +2,42 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
 	"project-quiz3/database"
 	"project-quiz3/router"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 var (
 	DB *sql.DB
+	err error
 )
 func main(){
-	db.Setup()
+	//db.Setup()
+	err = godotenv.Load("config/.env")
+	if err!=nil{
+		fmt.Println("Failed load file .env")
+	}else{
+		fmt.Println("Success read file .env")
+	}
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("PGHOST"),
+		os.Getenv("PGPORT"),
+		os.Getenv("PGUSER"),
+		os.Getenv("PGPASSWORD"),
+		os.Getenv("PGDATABASE"))
+	fmt.Println(psqlInfo)
+	DB, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	err = DB.Ping()
+	if err != nil {
+		panic(err)
+	}else{
+		fmt.Println("Successfully connected to DB")
+	}
 	db.DbMigrate(DB)
 	defer DB.Close()
 	app :=gin.Default()
